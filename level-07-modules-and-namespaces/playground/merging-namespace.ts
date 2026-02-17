0cd9e3e0aece8da1826a998f1450e48bed70d28b
N-Dave-08 <cdave.dev@gmail.com>
2026-02-17 23:00:42 +0800

    in progress: merging namespaces

---
+++ level-07-modules-and-namespaces/playground/merging-namespace.ts
@@ -0,0 +1,14 @@
+namespace Utils {
+	export function add(a: number, b: number) {
+		return a + b;
+	}
+}
+
+namespace Utils {
+	export function subtract(a: number, b: number) {
+		return a - b;
+	}
+}
+
+Utils.add(2, 2);
+Utils.subtract(3, 2);
